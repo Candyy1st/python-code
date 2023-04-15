0e8b656f2369df1efaa61174377bd2fa9f8c95be
Candyy1st <candradewa1981@gmail.com>
2023-04-15 11:27:57 +0700

    penambahan for nd while

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
-# with ssh
+jumlahBuku = 10
+print('Ibu : "Baca Bukumu"')
+
+bukuTerbaca = 0
+print(f"jumlah buku yang sudah dibaca = {bukuTerbaca}")
+
+while bukuTerbaca < jumlahBuku:
+    print(f"Buku ke-{bukuTerbaca+1}")
+    bukuTerbaca = bukuTerbaca +1
+
+print(f"jumlah buku yang sudah dibaca = {bukuTerbaca}")
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-"""
-Ini adalah demo project pertama dengan Python
-"""
-print("hello world")
+jumlahBuku = 10
+print('Ibu : "Baca Bukumu"')
+
+bukuTerbaca = 0
+
+for bukuTerbaca in range(1,11):
+    print(f"Buku ke {bukuTerbaca} sudah dibaca")
+
+print(f"jumlah buku yang sudah dibaca = {bukuTerbaca}")
